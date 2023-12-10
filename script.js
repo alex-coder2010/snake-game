19771a5c01c8b63ba1bf20d313e00f8b5f1cf854
user-name-user <135058132+user-name-user@users.noreply.github.com>
2023-12-10 21:05:55 +0100

    Update script.js
    Added restart function, gameover function and check if snake touches walls

--- script.js
+++ script.js
@@ -1,53 +1,88 @@
-// Create gameboard with grid
-let canvas = document.getElementById("gameBoard");
-let context = canvas.getContext("2d");
 
 // Variables
 
+const resetButton = document.querySelector("#button")
+let canvas = document.querySelector("#gameBoard");
+let context = canvas.getContext("2d");
+
 let snake = [
-  [0, 337, 26, 26],
-  [0, 337, 53, 26],
-  [0, 337, 78, 26],
-  [0, 337, 103, 26],
-  [0, 337, 130, 26],
-];
+                [0, 337, 26, 26],
+                [0, 363, 26, 26],
+                [0, 389, 26, 26],
+                [0, 415, 26, 26],
+                [0, 441, 26, 26],
+                [0, 467, 26, 26],
+                [0, 493, 26, 26],
+                [0, 519, 26, 26],
+                [0, 545, 26, 26],
+                [0, 571, 26, 26],
+                [0, 597, 26, 26],
+                [0, 623, 26, 26],
+
+
+                                                ];
+
+let snakeBody = [   
+                    [0, 337, 26, 26],
+                    [0, 363, 26, 26],
+                    [0, 389, 26, 26],
+                    [0, 415, 26, 26],
+                    [0, 441, 26, 26],
+                    [0, 467, 26, 26],
+                    [0, 493, 26, 26],
+                    [0, 519, 26, 26],
+                    [0, 545, 26, 26],
+                    [0, 571, 26, 26],
+                    [0, 597, 26, 26],          
+                                                ];
 
 let direction = "right";
 
-let speed = 200
+let gameRunning = true
+
+let speed = 100
 
 // Logic
 
 window.addEventListener("keydown", (event) => {
-  if (event.key == "ArrowDown") {
+
+  if (event.key == "ArrowDown" && direction != "up") {
     direction = "down";
-    console.log(direction);
   }
 
-  if (event.key == "ArrowUp") {
-    console.log(direction);
+  if (event.key == "ArrowUp"  && direction != "down") {
     direction = "up";
   }
 
-  if (event.key == "ArrowLeft") {
-    console.log(direction);
+  if (event.key == "ArrowLeft" && direction != "right") {
     direction = "left";
   }
 
-  if (event.key == "ArrowRight") {
-    console.log(direction);
+  if (event.key == "ArrowRight" && direction != "left") {
     direction = "right";
-  }
-});
 
-function drawSnake(snake) {
+  }});
+
+
+resetButton.addEventListener("click", () => {
+    resetButton.style.visibility = "hidden"
+    direction = "right"
+    snake = [[0, 337, 26, 26],[0, 363, 26, 26],[0, 389, 26, 26],[0, 415, 26, 26],[0, 441, 26, 26],[0, 467, 26, 26]]
+    context.beginPath()
+    context.fillStyle = "white"
+    context.fillRect(0, 0, 648, 648)
+    context.stroke()
+})
+
+function drawSnake() {
+  console.log(snake)
   context.beginPath();
   context.strokeStyle = "white";
   context.fillStyle = "darkgreen";
-  for (let i = 0; i < snake.length; i++) {
-    context.fillRect(snake[i][0], snake[i][1], snake[i][2], snake[i][3]);
-    context.strokeRect(snake[i][0], snake[i][1], snake[i][2], snake[i][3]);
-    context.strokeRect(snake[i][0], snake[i][1], snake[i][2], snake[i][3]);
+  for (let i = 0; i <= snake.length; i++) {
+    context.fillRect(snake[i][0], snake[i][1], 26, 26);
+    context.strokeRect(snake[i][0], snake[i][1], 26, 26);
+    context.strokeRect(snake[i][0], snake[i][1], 26, 26);
   }
   context.stroke();
 }
@@ -58,51 +93,76 @@ function clearTail(){
     context.fillStyle = "white";
     context.fillRect(snake[0][0], snake[0][1], snake[0][2], snake[0][3]);
     context.strokeRect(snake[0][0], snake[0][1], snake[0][2], snake[0][3]);
-    console.log(snake[0][0], snake[0][1], snake[0][2], snake[0][3])
-    for (let i = 0; i < 650; i += 26) {
-        context.moveTo(i, 0);
-        context.lineTo(i, 650);
-        context.moveTo(0, i);
-        context.lineTo(650, i);
-      }
     context.stroke();
     
 }
 
-// function checkCollisions(){
-//     if()
-// }
-
 function moveSnake() {
-
-    clearTail()
-  
     snake.shift();
+    snakeBody.shift();
+    snakeBody.push(snake[snake.length - 1])
 
     if (direction == "right") {
-        
-        snake.push([snake[snake.length - 1][0], snake[snake.length - 1][1], snake[snake.length - 1][2] + 26, snake[snake.length - 1][3]]);
+        snake.push([snake[snake.length - 1][0] + 26, snake[snake.length - 1][1], 26, 26]);
     }
 
     if (direction == "left") {
-        
-        snake.push([snake[snake.length - 1][0], snake[snake.length - 1][1], snake[snake.length - 1][2] - 26, snake[snake.length - 1][3]]);
+        snake.push([snake[snake.length - 1][0] - 26, snake[snake.length - 1][1], 26, 26]);
     }
 
     if (direction == "up") {
-        
-        snake.push([snake[snake.length - 1][0], snake[snake.length - 1][1] - 26, snake[snake.length - 1][2], snake[snake.length - 1][3]]);
-        console.log(snake)
+        snake.push([snake[snake.length - 1][0], snake[snake.length - 1][1] - 26, 26, 26]);
     }
 
     if (direction == "down") {
-        
-        snake.push([snake[snake.length - 1][0], snake[snake.length - 1][1] + 26, snake[snake.length - 1][2], snake[snake.length - 1][3]]);
+        snake.push([snake[snake.length - 1][0], snake[snake.length - 1][1] + 26, 26, 26]);
     }
 
-    drawSnake(snake);
+    drawSnake();
+
 }
+
+function gameOver(){
+    context.beginPath();
+    direction = "right"
+    context.font = 'italic 40pt Calibri';
+    context.fillStyle = "black"
+    context.textAlign = "center"
+    context.fillText("GAME OVER!", canvas.width / 2, canvas.height / 2)
+    context.stroke();
+    resetButton.style.visibility = "visible"
+}
+
+function checkCollisions(){
+
+    //big if statement that checks if the head of the snake touches any of the borders
+
+    if(snake[snake.length - 1][0] < -1 || snake[snake.length - 1][0] >= 630 || snake[snake.length - 1][1] < -1 || snake[snake.length - 1][1] >= 630){
+        snake = [snake[0][0], snake[0][1]]
+        gameOver()
+    }
+
+    // check if snake is colliding itself
+
+    for(let i = 0; i <= snakeBody.length; i++){
+        console.log(`SnakeBody at [i]: ${snakeBody[i]}, Snake[-1]: ${snake[snake.length -1]}`)
+        if(snake[snake.length - 1] == snakeBody[i]){
+            snake = [snake[0][0], snake[0][1]]
+            gameOver()
+        }
+    }
+
+        
+}
+
+resetButton.style.visibility = "hidden";
+
 
 setInterval(moveSnake, speed);
 setInterval(clearTail, speed);
+setInterval(checkCollisions, speed);
+
+
+
+
 
